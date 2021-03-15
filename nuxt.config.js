dfa2ee528af74ef49a4e00683dbd3016b99e9506
git@github.com:takuto0414/wildman.git <tamoumi0414@gmail.com>
2021-03-15 18:49:01 +0900

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,13 +27,8 @@ export default {
 
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
 
-    buildModules: [
-        // Simple usage
-        '@nuxtjs/vuetify',
+    buildModules: ["@nuxtjs/vuetify"],
 
-        // With options
-        ['@nuxtjs/vuetify', { /* module options */ }]
-    ],
 
 
     // Modules: https://go.nuxtjs.dev/config-modules
